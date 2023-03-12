f16b6614303297803160a6dafc198900fb1671f2
Varshant Dhar <varshantcricket@gmail.com>
2023-03-12 10:12:33 -0400

    SEUnit + mobilenet

--- conv/common.py
+++ conv/common.py
@@ -57,9 +57,49 @@ class LayerNorm(nn.Module):
     def forward(self, x):
         return F.layer_norm(x, self.weight.shape, self.weight, self.bias, 1e-5)
 
+class Classifier(nn.Module):
+    def __init__(self, in_channels, num_classes) -> None:
+        super().__init__()
+        self.conv = torch.nn.Conv2d(in_channels = 1,
+                                    out_channels = num_classes,
+                                    kernel_size = 1,
+                                    bias = True)
+        
+        self.flatten = Flatten()
+    
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.flatten.forward(x)
+        return x
+
+    def init_params(self):
+        nn.init.xavier_normal_(self.conv.weight, gain = 1.0)
+
+class SEUnit(nn.Module):
+    def __init__(self,
+                 channels,
+                 squeeze_factor = 16,
+                 squeeze_activation = "relu",
+                 excite_activation = "sigmoid") -> None:
+        super().__init__()
+        squeeze_channels = channels // squeeze_factor
+        self.pool = nn.AdaptiveAvgPool2d(output_size=1)
+        self.conv1 = conv1x1(in_channels=channels, out_channels=squeeze_channels, bias=True)
+        self.activation1 = get_activation(squeeze_activation)
+        self.conv2 = conv1x1(in_channels=squeeze_channels, out_channels=channels, bias=True)
+        self.activation2 = get_activation(excite_activation)
+
+    def forward(self, x):
+        z = self.pool(x)
+        z = self.conv1(z)
+        z = self.activation1(z)
+        z = self.conv2(z)
+        s = self.activation2(z)
+        return x * s
+
 
 def conv1x1(in_channels, out_channels, stride=1, bias=False):
-    return torch.nn.Conv2d(
+    return nn.Conv2d(
         in_channels = in_channels,
         out_channels = out_channels,
         kernel_size = 1,
@@ -69,7 +109,7 @@ def conv1x1(in_channels, out_channels, stride=1, bias=False):
     )
 
 def conv3x3(in_channels, out_channels, stride=1, bias=False):
-    return torch.nn.Conv2d(
+    return nn.Conv2d(
         in_channels = in_channels,
         out_channels = out_channels,
         kernel_size = 3,
@@ -79,7 +119,7 @@ def conv3x3(in_channels, out_channels, stride=1, bias=False):
     )
 
 def conv3x3_dw(channels, stride=1):
-    return torch.nn.Conv2d(
+    return nn.Conv2d(
         in_channels = channels,
         out_channels = channels,
         kernel_size = 3,
@@ -90,7 +130,7 @@ def conv3x3_dw(channels, stride=1):
     )
 
 def conv5x5_dw(channels, stride=1):
-    return torch.nn.Conv2d(
+    return nn.Conv2d(
         in_channels = channels,
         out_channels = channels,
         kernel_size = 5,
@@ -99,3 +139,120 @@ def conv5x5_dw(channels, stride=1):
         groups = channels,
         bias = False
     )
+
+class ConvBlock(nn.Module):
+    def __init__(self, 
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                groups = 1,
+                bias = False,
+                use_bn = True,
+                activation="relu") -> None:
+        super().__init__()
+        self.use_bn = use_bn
+        self.use_activation = (activation is not None)
+
+        self.conv = nn.Conv2d(
+            in_channels = in_channels,
+            out_channels = out_channels,
+            kernel_size = kernel_size,
+            stride = stride,
+            padding = padding,
+            groups = groups,
+            bias = bias
+        )
+
+        if self.use_bn:
+            self.bn = nn.BatchNorm2d(num_features=out_channels)
+        if self.use_activation:
+            self.activation = get_activation(activation)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.use_bn:
+            x = self.use_bn(x)
+        if self.use_activation:
+            x = self.activation(x)
+        return x
+
+def conv1x1_block(in_channels,
+                 out_channels,
+                 stride=1,
+                 bias=False,
+                 use_bn=True,
+                 activation="relu"):
+    return ConvBlock(in_channels = in_channels,
+                     out_channels = out_channels,
+                     kernel_size = 1,
+                     stride = stride,
+                     bias = bias,
+                     padding = 0,
+                     use_bn = use_bn,
+                     activation = activation)
+
+def conv3x3_block(in_channels,
+                 out_channels,
+                 stride=1,
+                 bias=False,
+                 use_bn=True,
+                 activation="relu"):
+    return ConvBlock(in_channels = in_channels,
+                     out_channels = out_channels,
+                     kernel_size = 3,
+                     stride = stride,
+                     padding = 1,
+                     stride = stride,
+                     bias = bias,
+                     use_bn = use_bn,
+                     activation = activation)
+
+def conv7x7_block(in_channels,
+                 out_channels,
+                 stride=1,
+                 bias=False,
+                 use_bn=True,
+                 activation="relu"):
+    return ConvBlock(in_channels = in_channels,
+                     out_channels = out_channels,
+                     kernel_size = 7,
+                     stride = stride,
+                     padding = 3,
+                     stride = stride,
+                     bias = bias,
+                     use_bn = use_bn,
+                     activation = activation)
+
+def conv3x3_dw_block(channels,
+                    stride = 1,
+                    bias = False,
+                    use_bn = True,
+                    activation = "relu"):
+    return ConvBlock (in_channels = channels,
+                     out_channels = channels,
+                     kernel_size = 3,
+                     groups = channels,
+                     stride = stride,
+                     bias = bias,
+                     padding = 1,
+                     use_bn = use_bn,
+                     activation = activation
+                     )
+
+def conv5x5_dw_block(channels,
+                    stride = 1,
+                    bias = False,
+                    use_bn = True,
+                    activation = "relu"):
+    return ConvBlock (in_channels = channels,
+                     out_channels = channels,
+                     kernel_size = 5,
+                     groups = channels,
+                     stride = stride,
+                     bias = bias,
+                     padding = 2,
+                     use_bn = use_bn,
+                     activation = activation
+                     )
---
+++ conv/mobilenet.py
@@ -0,0 +1,42 @@
+from site import USER_SITE
+from urllib.parse import uses_netloc
+import torch.nn as nn
+import torch.nn.init
+
+from common import conv1x1_block, conv3x3_block, conv3x3_dw_block, conv5x5_dw_block, SEUnit, Classifier
+
+
+
+class DepthwiseSeparableConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride) -> None:
+        super().__init__()
+        self.conv_dw = conv3x3_dw_block(channels=in_channels, stride=stride)
+        self.conv_pw = conv1x1_block(in_channels=in_channels, out_channels=out_channels)
+
+    def forward(self, x):
+        x = self.conv_dw(x)
+        z = self.conv_pw(x)
+        return z
+
+
+class LinearBottleneck(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, 
+                stride, activation="relu6", kernel_size=3, use_se=False) -> None:
+        super().__init__()
+        self.use_res_skip = (in_channels == out_channels) and (stride == 1)
+        self.use_se = use_se
+
+        self.conv1 = conv1x1_block(in_channels=in_channels, out_channels=mid_channels, 
+                                    activation=activation)
+        if kernel_size == 3:
+            self.conv2 = conv3x3_dw_block(channels=mid_channels, stride=stride, activation=activation)
+        elif kernel_size == 5:
+            self.conv2 = conv5x5_dw_block(channels=mid_channels, stride=stride, activation=activation)
+        else:
+            raise ValueError
+        if self.use_se:
+            self.se_unit = SEUnit(channels=mid_channels, squeeze_factor=4, squeeze_activation="relu", excite_activation="hsigmoid")
+        self.conv3 = conv1x1_block(in_channels=mid_channels, out_channels=out_channels, activation=None)
+
+
+
